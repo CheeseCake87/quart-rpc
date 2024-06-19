0008b6cea4bae1920a42124fc7827791ff52b0f6
David Carmichael <carmichaelits@gmail.com>
2024-06-19 09:03:35 +0100

    fix: module not found, was removed

--- quart_rpc/version_1_0/__init__.py
+++ quart_rpc/version_1_0/__init__.py
@@ -3,6 +3,5 @@ from .model import RPCModel
 from .request import RPCRequest
 from .response import RPCResponse
 from .rpc import RPC
-from ._pass_version_check import pass_version_check
 
-__all__ = ["RPC", "RPCResponse", "RPCModel", "RPCRequest", "RPCAuthSessionKey", "pass_version_check"]
+__all__ = ["RPC", "RPCResponse", "RPCModel", "RPCRequest", "RPCAuthSessionKey"]
